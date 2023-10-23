a55f1b256dcbdf7288529d46c3fb6a353f05605a
DJmoster <Zapotichnyi12@gmail.com>
2023-10-23 21:12:01 +0300

    Created welcome fragment

---
+++ app/src/main/java/com/walletsphere/app/fragments/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.walletsphere.app.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.walletsphere.app.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
