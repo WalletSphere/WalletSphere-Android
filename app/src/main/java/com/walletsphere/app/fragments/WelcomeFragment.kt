e5e319903fd6dd1a12f1f06cc9fba0e5b10d1e0d
DJmoster <Zapotichnyi12@gmail.com>
2023-10-27 23:24:11 +0300

    Finished Register and Login Fragments

--- app/src/main/java/com/walletsphere/app/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/walletsphere/app/fragments/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.walletsphere.app.databinding.FragmentWelcomeBinding
 
 
@@ -23,6 +24,19 @@ class WelcomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.registerButton.setOnClickListener {
+            findNavController()
+                .navigate(
+                    WelcomeFragmentDirections.actionWelcomeFragmentToRegisterFragment()
+                )
+        }
+
+        binding.loginButton.setOnClickListener {
+            findNavController()
+                .navigate(
+                    WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment()
+                )
+        }
     }
 
 }
